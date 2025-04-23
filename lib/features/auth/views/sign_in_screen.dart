5a8e3263f88ea4db59539126bd2362a5c99e2505
taskinnasif144 <taskinnasif144@gmail.com>
2025-04-23 08:17:53 +0600

    Add localization support and update sign-in screen
    - Introduced l10n.yaml for localization configuration.
    - Added localization keys for the sign-in screen in English and Spanish ARB files.
    - Updated SignInScreen to use localized text for the screen title.
    - Generated necessary CMake and build files for Android native support.

--- lib/features/auth/views/sign_in_screen.dart
+++ lib/features/auth/views/sign_in_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Sign In Screen')));
+    return Scaffold(body: Center(child: Text(AppLocalizations.of(context)!.signInScreen)));
   }
 }
