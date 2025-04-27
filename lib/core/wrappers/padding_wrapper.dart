86b5a216671ff0740de87a67e0b2fddbc5b63341
taskinnasif144 <taskinnasif144@gmail.com>
2025-04-27 16:28:03 +0600

    refactor: Rename PaddingWrapper to ScreenWrapper and enhance layout with app bar support

--- lib/core/wrappers/padding_wrapper.dart
+++ lib/core/wrappers/padding_wrapper.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class PaddingWrapper extends StatelessWidget {
-  const PaddingWrapper({super.key, required this.child});
+class ScreenWrapper extends StatelessWidget {
+  const ScreenWrapper({super.key, required this.child, this.appbar});
+  final PreferredSizeWidget? appbar;
   final Widget child;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
-      child: child,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: appbar,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+          child: child,
+        ),
+      ),
     );
   }
 }
