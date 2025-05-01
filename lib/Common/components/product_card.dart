c9d72f9278dad30425e848b8499a78eb850e9464
taskinnasif144 <taskinnasif144@gmail.com>
2025-05-01 22:45:53 +0600

    feat: Update ProductCard component with improved layout, added product tags, and pricing display

--- lib/Common/components/product_card.dart
+++ lib/Common/components/product_card.dart
@@ -3,9 +3,11 @@ import 'package:flutter_getx_template/core/constants/app_colors.dart';
 import 'package:flutter_getx_template/core/utils/image_renderer.dart';
 import 'package:flutter_rating/flutter_rating.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 const image =
     "https://images.unsplash.com/photo-1523275335684-37898b6baf30?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D";
+const tags = ["Photography", "Camera", "DSLR", "Professional", "Gadget"];
 
 class ProductCard extends StatelessWidget {
   const ProductCard({super.key});
@@ -18,14 +20,14 @@ class ProductCard extends StatelessWidget {
         border: Border.all(color: AppColors.lightGray),
       ),
       width: 155.w,
-      padding: EdgeInsets.all(5),
+      padding: EdgeInsets.all(8),
       child: Column(
         spacing: 10,
         children: [
           // the image of the product
           SizedBox(
-            height: 180.h,
-            child: imageRender(url: image, width: 150, height: 180),
+            height: 150.h,
+            child: imageRender(url: image, width: 150, height: 150),
           ),
           // has the rating and the category
           Row(
@@ -55,9 +57,54 @@ class ProductCard extends StatelessWidget {
             ).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.bold),
           ),
 
-          // Product tags 
-          
+          // Product tags
+          Text(
+            tags.join(', '),
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+
+          Divider(color: AppColors.lightGray,),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // if it has discount then two prices will be displayed
+              Column(
+                spacing: 2,
+                children: [
+                  Text("\$89.99", style: Theme.of(context).textTheme.bodyMedium,),
+                  Text("\$89.99", style: Theme.of(context).textTheme.bodySmall!.copyWith(decoration: TextDecoration.lineThrough), ),
+                ],
+              ),
+              // for buy now and add to cart button
+              Container(
+                padding: EdgeInsets.only(left: 5),
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: AppColors.lightGray),
+                    left: BorderSide(color: AppColors.lightGray),
+                    bottom: BorderSide(color: AppColors.lightGray),
+                  ),
+                  borderRadius: BorderRadius.circular(100)
+                ),
+                child: Row(
+                  spacing: 4,
+                  children: [
+                    Text("Buy Now", style: Theme.of(context).textTheme.bodySmall,),
+                    Container(
+                      padding: EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: AppColors.warningColor),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(Icons.add, size: 16,),),
+                  ],
+                ),
+              )
+            ],
+          )
         ],
+
       ),
     );
   }
