d9c29ad9a78cfee362ec3e2fa5525bde30bb1fa7
taskinnasif144 <taskinnasif144@gmail.com>
2025-05-04 14:03:10 +0600

    feat: add id field to ProductModel and update best selling and hot deals products

--- lib/Common/Controllers/product_controllers.dart
+++ lib/Common/Controllers/product_controllers.dart
@@ -4,25 +4,24 @@ import 'package:get/get.dart';
 class ProductControllers extends GetxController {
   // List of best selling products
   RxList<ProductModel> bestSellingProducts = <ProductModel>[
-    ProductModel(productTitle: "Smart Watch", rating: 3.5, reviewerCount: 120, currentPrice: 100),
-    ProductModel(productTitle: "Proffesional DSLR Camera", rating: 4.5, reviewerCount: 320, currentPrice: 400, discountedFromPrice: 530),
-    ProductModel(productTitle: "Compact Travel Camera", rating: 3.5, reviewerCount: 120, currentPrice: 100),
-    ProductModel(productTitle: "Bluetooth Earbuda", rating: 3.5, reviewerCount: 120, currentPrice: 100),
-    ProductModel(productTitle: "Gaming Headset", rating: 3.5, reviewerCount: 120, currentPrice: 100),
-    ProductModel(productTitle: "Caliburn Pod", rating: 3.5, reviewerCount: 120, currentPrice: 100),
-    ProductModel(productTitle: "Gaming Mouse", rating: 3.5, reviewerCount: 120, currentPrice: 100),
+    ProductModel(id: 1, productTitle: "Smart Watch", rating: 3.5, reviewerCount: 120, currentPrice: 100),
+    ProductModel(id: 2, productTitle: "Proffesional DSLR Camera", rating: 4.5, reviewerCount: 320, currentPrice: 400, discountedFromPrice: 530),
+    ProductModel(id: 3, productTitle: "Compact Travel Camera", rating: 3.5, reviewerCount: 120, currentPrice: 100),
+    ProductModel(id: 4, productTitle: "Bluetooth Earbuda", rating: 3.5, reviewerCount: 120, currentPrice: 100),
+    ProductModel(id: 5, productTitle: "Gaming Headset", rating: 3.5, reviewerCount: 120, currentPrice: 100),
+    ProductModel(id: 6, productTitle: "Caliburn Pod", rating: 3.5, reviewerCount: 120, currentPrice: 100),
+    ProductModel(id: 7, productTitle: "Gaming Mouse", rating: 3.5, reviewerCount: 120, currentPrice: 100),
   ].obs;
 
   // List of hot deals products
   RxList<ProductModel> hotDealsProducts = <ProductModel>[
-    ProductModel(productTitle: "Wireless Charger", rating: 4.0, reviewerCount: 150, currentPrice: 50),
-    ProductModel(productTitle: "Noise Cancelling Headphones", rating: 4.8, reviewerCount: 200, currentPrice: 300, discountedFromPrice: 350),
-    ProductModel(productTitle: "Smartphone Gimbal", rating: 4.2, reviewerCount: 180, currentPrice: 120),
-    ProductModel(productTitle: "Portable Power Bank", rating: 4.5, reviewerCount: 250, currentPrice: 80),
-    ProductModel(productTitle: "4K Action Camera", rating: 4.3, reviewerCount: 170, currentPrice: 200),
-    ProductModel(productTitle: "Smart Home Speaker", rating: 4.6, reviewerCount: 300, currentPrice: 150),
-    ProductModel(productTitle: "Ergonomic Keyboard", rating: 4.4, reviewerCount: 220, currentPrice: 90),
+    ProductModel(id: 8, productTitle: "Wireless Charger", rating: 4.0, reviewerCount: 150, currentPrice: 50),
+    ProductModel(id: 9, productTitle: "Noise Cancelling Headphones", rating: 4.8, reviewerCount: 200, currentPrice: 300, discountedFromPrice: 350),
+    ProductModel(id: 10, productTitle: "Smartphone Gimbal", rating: 4.2, reviewerCount: 180, currentPrice: 120),
+    ProductModel(id: 11, productTitle: "Portable Power Bank", rating: 4.5, reviewerCount: 250, currentPrice: 80),
+    ProductModel(id: 12, productTitle: "4K Action Camera", rating: 4.3, reviewerCount: 170, currentPrice: 200),
+    ProductModel(id: 13, productTitle: "Smart Home Speaker", rating: 4.6, reviewerCount: 300, currentPrice: 150),
+    ProductModel(id: 14, productTitle: "Ergonomic Keyboard", rating: 4.4, reviewerCount: 220, currentPrice: 90),
   ].obs;
-  
   
 }
--- lib/Common/models/product_model.dart
+++ lib/Common/models/product_model.dart
@@ -1,4 +1,5 @@
 class ProductModel {
+  final int id;
   final String imageUrl;
   final String productTitle;
   final double rating;
@@ -6,6 +7,7 @@ class ProductModel {
   final double currentPrice;
   final double? discountedFromPrice;
   ProductModel({
+    required this.id,
      this.imageUrl = "https://images.unsplash.com/photo-1523275335684-37898b6baf30?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
     required this.productTitle,
     required this.rating,
